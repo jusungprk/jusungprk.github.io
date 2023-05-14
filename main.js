2b2894db4d79079f8523f2ab4694e97d1d40787a
Jusung Park <jusung.park.25@dartmouth.edu>
2023-05-13 22:23:59 -0400

    edits

--- main.js
+++ main.js
@@ -19,3 +19,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         star.classList.remove("animate");
       }, 1000);
     });
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    
+    var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
